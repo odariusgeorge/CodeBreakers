1958819962fa325c2dfba4445cb1bc7c3b93dc5f
andreiramescu <dark.gige@gmail.com>
2019-10-22 21:29:20 +0200

    Nice now we have a basic bluetooth connection (read the documentation its really really useful) Ps: the TODO List for now is of the professor so don't worry

--- app/src/main/java/com/example/codebreakers/MainActivity.java
+++ app/src/main/java/com/example/codebreakers/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.codebreakers;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 import it.unive.dais.legodroid.lib.EV3;
 import it.unive.dais.legodroid.lib.comm.BluetoothConnection;
 
 public class MainActivity extends AppCompatActivity {
+    public String deviceName = "Mark7";
+    public BluetoothConnection bt = new BluetoothConnection(deviceName);
+    public BluetoothConnection.BluetoothChannel channel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+           channel =  bt.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        channel.close();
+
+    }
+
+
 }
