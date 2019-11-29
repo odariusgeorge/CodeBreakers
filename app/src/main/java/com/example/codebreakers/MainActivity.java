c5e297dea881ce60d6f942931147c68030a69b68
Darius-George Oanea <george@fun-monkeys.com>
2019-11-29 12:27:34 +0100

    spaghetti

--- app/src/main/java/com/example/codebreakers/MainActivity.java
+++ app/src/main/java/com/example/codebreakers/MainActivity.java
@@ -5,9 +5,14 @@ import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.*;
+import android.text.Editable;
+import android.widget.EditText;
+import android.text.TextWatcher;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.IdRes;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
@@ -34,6 +39,7 @@ import it.unive.dais.legodroid.lib.plugs.TachoMotor;
 import it.unive.dais.legodroid.lib.plugs.UltrasonicSensor;
 import it.unive.dais.legodroid.lib.util.Prelude;
 import it.unive.dais.legodroid.lib.util.ThrowingConsumer;
+import it.unive.dais.legodroid.lib.util.Consumer;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_PERMISSION_CODE=100;
@@ -50,6 +56,17 @@ public class MainActivity extends AppCompatActivity {
     private static TachoMotor motorLeft;
     private static TachoMotor motorRight;
     private static TachoMotor motorClaws;
+    private int[][] matrix;
+    private Integer n;
+    private Integer m;
+    private Integer xBallValue;
+    private Integer yBallValue;
+    private Integer xRobotValue;
+    private Integer yRobotValue;
+    //    EditText xRobot = findViewById(R.id.xRobotStart);
+//    EditText yRobot = findViewById(R.id.yRobotStart);
+//    EditText xBall = findViewById(R.id.xStartBall);
+//    EditText yBall = findViewById(R.id.yStartBall);
     private void updateStatus(@NonNull Plug p, String key, Object value) {
         Log.d(TAG, String.format("%s: %s: %s", p, key, value));
         statusMap.put(key, value);
@@ -96,6 +113,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    int[][] constructMatrix(int n, int m) {
+        int [][] matrix = new int[n][m];
+        return matrix;
+    }
+
     private void setUpCamera() {
         if (!OpenCVLoader.initDebug()) {
             Log.e(TAG, "Unable to load OpenCV");
@@ -180,43 +203,51 @@ public class MainActivity extends AppCompatActivity {
 //            while (!api.ev3.isCancelled()) {
             while (i<1) {
                 try {
-                    Future<Short> ambient = lightSensor.getAmbient();
-                    updateStatus(lightSensor, "ambient", ambient.get());
-
-                    Future<Short> reflected = lightSensor.getReflected();
-                    updateStatus(lightSensor, "reflected", reflected.get());
-
-                    Future<Float> distance = ultraSensor.getDistance();
-                    updateStatus(ultraSensor, "distance", distance.get());
-
-                    Future<LightSensor.Color> colf = lightSensor.getColor();
-                    LightSensor.Color col = colf.get();
-                    updateStatus(lightSensor, "color", col);
-                    runOnUiThread(() -> findViewById(R.id.colorView).setBackgroundColor(col.toARGB32()));
-
-                    Future<Float> posMLeft = motorLeft.getPosition();
-                    updateStatus(motorLeft, "motor position", posMLeft.get());
-
-                    Future<Float> speedMLeft = motorLeft.getSpeed();
-                    updateStatus(motorLeft, "motor speed", speedMLeft.get());
-                    Future<Float> posMRight = motorRight.getPosition();
-                    updateStatus(motorRight, "motor position", posMRight.get());
-                    Future<Float> speedMRight = motorRight.getSpeed();
-                    updateStatus(motorRight, "motor speed", speedMRight.get());
-                    Future<Float> postMClaws = motorClaws.getPosition();
-                    updateStatus(motorClaws, "motor position", postMClaws.get());
-                    Future<Float> speedMClawst = motorClaws.getSpeed();
-                    updateStatus(motorRight, "motor speed", speedMClawst.get());
-                    if(center != null) {
-                        catchBall();
-                        api.soundTone(100,100,3000);
+                    int x = 0;
+                    int y = 0;
+                    while(x < n) {
                         goForward();
-                        releaseBall();
-                        goBack();
-                        stopMotors();
+                        x++;
                     }
+//                    Future<Short> ambient = lightSensor.getAmbient();
+//                    updateStatus(lightSensor, "ambient", ambient.get());
+//
+//                    Future<Short> reflected = lightSensor.getReflected();
+//                    updateStatus(lightSensor, "reflected", reflected.get());
+//
+//                    Future<Float> distance = ultraSensor.getDistance();
+//                    updateStatus(ultraSensor, "distance", distance.get());
+//
+//                    Future<LightSensor.Color> colf = lightSensor.getColor();
+//                    LightSensor.Color col = colf.get();
+//                    updateStatus(lightSensor, "color", col);
+//                    runOnUiThread(() -> findViewById(R.id.colorView).setBackgroundColor(col.toARGB32()));
+//
+//                    Future<Float> posMLeft = motorLeft.getPosition();
+//                    updateStatus(motorLeft, "motor position", posMLeft.get());
+//
+//                    Future<Float> speedMLeft = motorLeft.getSpeed();
+//                    updateStatus(motorLeft, "motor speed", speedMLeft.get());
+//                    Future<Float> posMRight = motorRight.getPosition();
+//                    updateStatus(motorRight, "motor position", posMRight.get());
+//                    Future<Float> speedMRight = motorRight.getSpeed();
+//                    updateStatus(motorRight, "motor speed", speedMRight.get());
+//                    Future<Float> postMClaws = motorClaws.getPosition();
+//                    updateStatus(motorClaws, "motor position", postMClaws.get());
+//                    Future<Float> speedMClawst = motorClaws.getSpeed();
+//                    updateStatus(motorRight, "motor speed", speedMClawst.get());
+//                    if(center != null) {
+//                        catchBall();
+//                        api.soundTone(100,100,3000);
+//                        goForward();
+//                        releaseBall();
+//                        goBack();
+//                        stopMotors();
+//                    }
                     i++;
-                } catch (IOException | InterruptedException | ExecutionException e) {
+                } catch (IOException e)
+//                        | InterruptedException | ExecutionException e) {
+                {
                     e.printStackTrace();
                 }
             }
@@ -229,6 +260,11 @@ public class MainActivity extends AppCompatActivity {
         final String TAG = Prelude.ReTAG("legoMainCustomApi");
         api.mySpecialCommand();
         legoMain(api);
+        EditText rows = findViewById(R.id.numberOfRows);
+        EditText columns = findViewById(R.id.numberOfColumns);
+        n = Integer.valueOf(rows.getText().toString());
+        m = Integer.valueOf(columns.getText().toString());
+        matrix = constructMatrix(n,m);
     }
 
 
